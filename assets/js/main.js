04c750fb5870514eb1a5b88f6b0abac1b62c8cd9
Mohammad Ibrahim <md.kardi420@gmail.com>
2017-04-13 01:28:22 -0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+       $(".vedio-play-btn").magnificPopup({
+
+       		type:'vedio',
+       });
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
